a93aff9eb20476b4137ca76f32b802ec1b02f2f8
melanyzelaya <melanyzelaya95@gmail.com>
2020-10-30 15:23:20 -0600

    primera parte de la Guia6

---
+++ Guia6_1714062014/ejemploMVC/ejemploMVC/Controllers/PruebaController.cs
@@ -0,0 +1,28 @@
+﻿using ejemploMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ejemploMVC.Controllers
+{
+    public class PruebaController : Controller
+    {
+        // GET: Prueba
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //POST: Prueba/Index
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Calculo obCalculo)
+        {
+            //Suma
+            int resultado = obCalculo.numero1 + obCalculo.numero2;
+            ViewBag.resultado = resultado;
+            return View(obCalculo);
+        }
+    }
+}
---
+++ Guia6_1714062014/ejemploMVC/ejemploMVC/Models/Calculo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ejemploMVC.Models
+{
+    public class Calculo
+    {
+        public int numero1 { get; set; }
+        public int numero2 { get; set; }
+    }
+}
